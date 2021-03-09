2b32616ac8bfaa2c4a3d55d47c29c42107958c95
carrillojeisson2 <carrillojeisson2@gmail.com>
2021-03-09 05:19:08 -0500

    Seccion 12 - Gogole

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcrpt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verifity');
 
 const login = async (req, res = response) => {
 
@@ -46,6 +47,56 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+
+        const { name, email, picture } = await googleVerify(googleToken);
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if (!usuarioDB) {
+            // si no existe el usuario
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            // exite usuario
+            usuario = usuarioDB;
+            usuario.google = true
+        }
+
+        // guardar en DB
+        await usuario.save();
+
+        // Generar token
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            token
+
+        });
+
+    } catch (error) {
+
+        res.status(404).json({
+            ok: false,
+            msg: 'Token no es correcto'
+        })
+    }
+
+
+}
+
 module.exports = {
     login,
+    googleSignIn
 }
--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use(express.json());
 // base de datos
 dbConnection();
 
+// directorio publico
+app.use(express.static('public'));
+
 // rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/hospitales', require('./routes/hospitales'));
